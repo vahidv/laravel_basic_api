94309b76a24423fb1b3ca94c9c00801454a1fde8
vahid vahedi <vahedivahid7@gmail.com>
2018-04-18 22:55:46 +0430

    first initializing commit

---
+++ app/Http/Requests/RegisterFormRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|unique:users',
+            'author_name' => 'required|string|unique:users',
+            'username' => 'required|string|unique:users',
+            'family' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6|max:10',
+        ];
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+Route::post('signup', 'AuthController@register');
+
+Route::post('login', 'AuthController@login');
+
+
+Route::group(['middleware' => 'jwt.auth'], function(){
+    Route::post('auth/logout', 'AuthController@logout');
+});
+
+
+Route::middleware('jwt.refresh')->get('/token/refresh', 'AuthController@refresh');
